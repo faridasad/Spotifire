06cf6bd820d0713cd4327c70b1edfdee05522d85
faridasad <asadlifaridd@gmail.com>
2023-06-02 20:57:46 +0400

    Add Navigation and Library section onto Sidebar

--- src/components/LeftSidebar/index.tsx
+++ src/components/LeftSidebar/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRef } from "react";
 import styles from "./left-sidebar.module.scss";
+import Link from "next/link";
+import Icon from "../Icons";
 
 const MAX_WIDTH = 351;
 const MIN_WIDTH = 71;
@@ -68,7 +70,61 @@ const LeftSidebar = () => {
 
   return (
     <div className={styles.aside} ref={leftSidebarRef}>
-      <div className={styles.leftSidebar}></div>
+      <div className={styles.left_sidebar}>
+        <div className={styles.navigation}>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">
+                  <Icon name="home" /> Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <Icon name="search" /> Search
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.library}>
+          <header className={styles.header}>
+            <button className={styles.library__toggler}>
+              <Icon name="library" />
+              Your Library
+            </button>
+            <div className={styles.utils}>
+              <button>
+                <Icon name="plus" size={16} />
+              </button>
+              <button>
+                <Icon name="navigationNext" size={16} />
+              </button>
+            </div>
+          </header>
+          <div className={styles.categories}>
+            <button>
+              <span>Playlists</span>
+            </button>
+            <button>
+              <span>Albums</span>
+            </button>
+            <button>
+              <span>Podcasts & Shows</span>
+            </button>
+          </div>
+          <div className={styles.lib_content}>
+            <div className={styles.search_filter}>
+              <button>
+                <Icon name="search" size={16} />
+              </button>
+              <div className={styles.dropdown}>
+                Recently Added <Icon name="dropDown" size={16} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className={styles.resizeHandle} onMouseDown={startResize}></div>
     </div>
   );
