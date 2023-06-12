9786f6c150056cb35e232abf5c6d46a03bf74852
faridasad <asadlifaridd@gmail.com>
2023-06-12 23:07:39 +0400

    Now playing section

--- src/components/NowPlaying/index.tsx
+++ src/components/NowPlaying/index.tsx
@@ -1,8 +1,69 @@
 import styles from "./now-playing.module.scss"
+import Icon from "../Icons";
+import Player from "./Player";
+import VolumeBar from "./VolumeBar";
+import "./now-playing.module.scss";
+import Image from "next/image";
 
 const NowPlaying = () => {
   return (
-    <div className={styles.now_playing}></div>
+    <footer className={styles.footer}>
+      <div className={styles.footer_wrapper}>
+        <div className={styles.footer_wrapper__left}>
+          <div className={styles.audio_image}>
+            <Image src="https://i.scdn.co/image/ab67616d00004851d4be605a267df4e9a09f6245" width={56} height={56} alt=""/>
+          </div>
+          <div className={styles.audio_info}>
+            <div className={styles.audio_info__title}>Blow Horn Joint</div>
+            <div className={styles.audio_info__artist}>DJ Premier</div>
+          </div>
+          <button role="switch" aria-checked>
+            <Icon name="heart" size={16} />
+          </button>
+        </div>
+        <div className={styles.footer_wrapper__center}>
+          <div className={styles.player_controls}>
+            <div className={styles.player_controls__left}>
+              <button role="switch">
+                <Icon name="shuffle" size={16} />
+              </button>
+              <button>
+                <Icon name="previous" size={16} />
+              </button>
+            </div>
+            <div className={styles.player_controls__play}>
+              <button>
+                <Icon name="play" size={16} />
+              </button>
+            </div>
+            <div className={styles.player_controls__right}>
+              <button>
+                <Icon name="next" size={16} />
+              </button>
+              <button role="switch">
+                <Icon name="repeat" size={16} />
+              </button>
+            </div>
+          </div>
+          <div className={styles.audio_player}>
+            <Player />
+          </div>
+        </div>
+        <div className={styles.footer_wrapper__right}>
+          <button role="switch">
+            <Icon name="queue" size={16} />
+          </button>
+          <div className={styles.volume_con}>
+            <button role="switch">
+              <Icon name="volume" size={16} />
+            </button>
+            <div className={styles.volume_con__slider}>
+              <VolumeBar />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
