bbf7cce8a7c778bab8764de632371be5b478ad34
faridasad <asadlifaridd@gmail.com>
2023-06-14 19:13:34 +0400

    Apply Topbar in Main view

--- src/components/MainView/index.tsx
+++ src/components/MainView/index.tsx
@@ -1,9 +1,12 @@
+import Topbar from "../Topbar"
 import styles from "./main-view.module.scss"
 
 const MainView = () => {
   return (
     <div className={styles.main_view}>
-
+      <Topbar>
+        
+      </Topbar>
     </div>
   )
 }
