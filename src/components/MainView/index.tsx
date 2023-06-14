6c84fafc4c66f4931dcb8b5787236accf0e27f60
faridasad <asadlifaridd@gmail.com>
2023-06-14 22:02:27 +0400

    Main view

--- src/components/MainView/index.tsx
+++ src/components/MainView/index.tsx
@@ -1,14 +1,18 @@
-import Topbar from "../Topbar"
-import styles from "./main-view.module.scss"
+"use client";
+import Topbar from "../Topbar";
+import styles from "./main-view.module.scss";
+import { usePathname } from "next/navigation";
 
-const MainView = () => {
+const MainView = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
   return (
     <div className={styles.main_view}>
       <Topbar>
-        
+        {children}
       </Topbar>
     </div>
   )
-}
+};
 
-export default MainView
+export default MainView;
