787365856c0ccb8c038336ff10d68000ad1bd8b0
faridasad <asadlifaridd@gmail.com>
2023-06-14 22:01:22 +0400

    Update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,37 @@
-import './styles/globals.scss'
-import { Roboto } from 'next/font/google'
+import "./styles/globals.scss";
+import { Roboto } from "next/font/google";
+import LeftSidebar from "@/components/LeftSidebar";
+import styles from "./page.module.scss";
+import MainView from "@/components/MainView";
+import NowPlaying from "@/components/NowPlaying";
 
 export const roboto = Roboto({
-  weight: ['300', '400', '500', '700', '900'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto'
+  weight: ["300", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
 });
 
 export const metadata = {
-  title: 'SpotiFire',
-  description: 'Spotify clone built with Next.js',
-}
+  title: "SpotiFire",
+  description: "Spotify clone built with Next.js",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={`${roboto.variable}`}>
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <div className={styles.rootGrid}>
+          <LeftSidebar />
+          <MainView>{children}</MainView>
+          <NowPlaying />
+        </div>
+      </body>
     </html>
-  )
+  );
 }
