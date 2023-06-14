74fc2dc7d2523d0e170e44acb0a372629cf32114
faridasad <asadlifaridd@gmail.com>
2023-06-14 22:02:12 +0400

    Add main view layout

---
+++ src/components/MainView/layout.tsx
@@ -0,0 +1,10 @@
+import styles from "./main-view.module.scss";
+import Topbar from "../Topbar";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+      {children}
+  );
+};
+
+export default MainLayout;
