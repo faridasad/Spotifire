3fd280934fe15b329c3b03a530d7bd71bb79b2d1
faridasad <asadlifaridd@gmail.com>
2023-06-14 22:01:38 +0400

    Home based on routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import LeftSidebar from '@/components/LeftSidebar'
-import styles from './page.module.scss'
-import MainView from '@/components/MainView'
-import NowPlaying from '@/components/NowPlaying'
-
 export default function Home() {
   return (
-    <div className={styles.rootGrid}>
-      <LeftSidebar />
-      <MainView />
-      <NowPlaying />
-    </div>
+    <div>Home</div>
   )
 }
