702de2abe3e483c33a01dc971895bfe960c0c395
faridasad <asadlifaridd@gmail.com>
2023-06-14 22:02:37 +0400

    Add Search Bar

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,21 @@
+import Icon from "../Icons"
+import styles from "./searchbar.module.scss"
+
+function SearchBar() {
+  return (
+    <div className={styles.search_bar}>
+      <div>
+        <span>
+          <Icon name="search" size={24} />
+        </span>
+      </div>
+      <input
+        type="text"
+        placeholder="What do you want to listen to?"
+        spellCheck="false"
+      />
+    </div>
+  )
+}
+
+export default SearchBar
