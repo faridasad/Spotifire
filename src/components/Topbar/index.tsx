a5e712603d0fab46571920cefb3c5049c30998f5
faridasad <asadlifaridd@gmail.com>
2023-07-14 12:37:30 -0700

    Remove later ui component

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import { Menu } from "@headlessui/react";
 import Icon from "../Icons";
@@ -19,49 +19,9 @@ const Topbar = ({children} : {children: ReactNode}) => {
         </button>
       </div>
       <div className={styles.child_comp}>{children !== null && children}</div>
-      {true ? (
-        <a className={styles.login_button}>
-          <span>Login</span>
-        </a>
-      ) : (
-        <Menu as="div" className={styles.profile_con}>
-          <Menu.Button className={styles.user_profile}>
-            <Image src="https://i.scdn.co/image/ab6775700000ee859e36edc9c5e37682327654e2" width={28} height={28} alt=""/>
-            <span>fared.wav</span>
-            <Icon name="dropDown" size={16} />
-          </Menu.Button>
-          <Menu.Items className={styles.menu_items}>
-            <Menu.Item>
-              {({ active }) => (
-                <a className={`${active && styles.active}`}>
-                  <span>Account</span>
-                  <Icon name="externalLink" size={16} />
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a className={`${active && styles.active}`}>Profile</a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a className={`${active && styles.active}`}>Settings</a>
-              )}
-            </Menu.Item>
-            <span className={styles.menu_divider}></span>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && styles.active}`}
-                >
-                  Log Out
-                </a>
-              )}
-            </Menu.Item>
-          </Menu.Items>
-        </Menu>
-      )}
+      <button className={styles.login_button} onClick={() => {signIn('spotify', {callbackUrl: "/"})}}>
+        <span>Login</span>
+      </button>
     </header>
   )
 }
