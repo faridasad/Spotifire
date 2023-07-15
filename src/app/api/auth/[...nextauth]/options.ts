a711c17577bc1d7af442ea94ddca05b982a4965c
faridasad <asadlifaridd@gmail.com>
2023-07-15 13:59:20 +0400

    Sign in and sign out

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,30 @@
 import type { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { AUTH_URL } from "../../../../../lib/spotify";
+import spotifyApi, { AUTH_URL } from "../../../../../lib/spotify";
+
+const refreshAccessToken = async (token: any) => {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      refreshedToken: refreshedToken.refresh_token ?? token.refreshToken
+    }
+
+  } catch (err) {
+    console.error(err);
+
+    return {
+      ...token,
+      error: "AccessTokenRefreshError",
+    };
+  }
+};
 
 export const options: NextAuthOptions = {
   providers: [
@@ -11,4 +35,34 @@ export const options: NextAuthOptions = {
     }),
   ],
   secret: process.env.JWT_SECRET,
+  callbacks: {
+    async jwt({ token, account, user }: any) {
+      //initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpiresAt: account.expires_at! * 1000,
+        };
+      }
+
+      // Return existing token if token is valid
+      if (Date.now() < token.accessTokenExpiresAt) {
+        return token;
+      }
+
+      // Refresh token
+
+      return await refreshAccessToken(token);
+    },
+  },
+  async session({session, token} : any){
+    session.user.accessToken = token.accessToken
+    session.user.refreshToken = token.refreshToken
+    session.user.username = token.username
+
+    return session;
+  }
 };
--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
+import { signIn, signOut } from "next-auth/react";
 import { Menu } from "@headlessui/react";
 import Icon from "../Icons";
 import { ReactNode } from "react";
 import Image from "next/image";
-import styles from "./topbar.module.scss"
+import styles from "./topbar.module.scss";
 
-
-const Topbar = ({children} : {children: ReactNode}) => {
+const Topbar = ({ children }: { children: ReactNode }) => {
   return (
     <header className={styles.header}>
       <div className={styles.navigation_arrows}>
-        <button name="Go Prev">
+        <button name="Go Prev" onClick={() => signOut()}>
           <Icon name="navigationPrev" size={16} />
         </button>
         <button name="Go Forward" disabled>
@@ -19,11 +19,16 @@ const Topbar = ({children} : {children: ReactNode}) => {
         </button>
       </div>
       <div className={styles.child_comp}>{children !== null && children}</div>
-      <button className={styles.login_button} onClick={() => {signIn('spotify', {callbackUrl: "/"})}}>
+      <button
+        className={styles.login_button}
+        onClick={() => {
+          signIn("spotify", { callbackUrl: "/" });
+        }}
+      >
         <span>Login</span>
       </button>
     </header>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
