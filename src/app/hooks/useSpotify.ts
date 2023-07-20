95f629e610aba01d3035aab9819aa886da8cabc0
faridasad <asadlifaridd@gmail.com>
2023-07-20 20:30:35 +0400

    Get spotifyApi from lib

--- src/app/hooks/useSpotify.ts
+++ src/app/hooks/useSpotify.ts
@@ -2,14 +2,10 @@
 
 import { signIn, useSession } from "next-auth/react";
 import React, { useEffect } from "react";
-import SpotifyWebApi from "spotify-web-api-node";
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-});
+import spotifyApi from "../../../lib/spotify";
 
 const useSpotify = () => {
+  
   const { data: session } = useSession();
 
   useEffect(() => {
