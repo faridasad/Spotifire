1216e7d77ed91637d2dbc9f89268eed0d58e247e
faridasad <asadlifaridd@gmail.com>
2023-07-20 20:27:29 +0400

    Add various image hosts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains: ['i.scdn.co', 'hips.hearstapps.com', 'upload.wikimedia.org']
+        domains: ['i.scdn.co', 'hips.hearstapps.com', 'upload.wikimedia.org', 'images-ak.spotifycdn.com', 'mosaic.scdn.co']
     }
 }
 
