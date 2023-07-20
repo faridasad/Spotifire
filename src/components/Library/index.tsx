6bd979a737313b997136e25c5ac40086bcd030a0
faridasad <asadlifaridd@gmail.com>
2023-07-20 20:35:39 +0400

    Settle user playlists

--- src/components/Library/index.tsx
+++ src/components/Library/index.tsx
@@ -1,45 +1,63 @@
-
-"use client"
-import Image from "next/image";
+"use client";
 import styles from "./library.module.scss";
-import spotifyApi from "../../../lib/spotify";
-import { signIn, useSession } from "next-auth/react";
-import { Session, getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation";
+import useSpotify from "@/app/hooks/useSpotify";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Icon from "../Icons";
 
+export default function Library() {
+  const spotifyApi = useSpotify();
+  const [playlists, setPlaylists] = useState([]);
+  const { data: session } = useSession();
 
-export default function Library(){
-
-  const {data: session} = useSession({
-    required: true,
-    /* onUnauthenticated(){
-      redirect('/api/auth/signin/spotify')
-    } */
-  }) 
-
-  console.log(session)
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
 
   return (
     <div className={styles.library}>
-      {/* <ul role="list">
-        {dummyData.map((i) => {
+      <div className={styles.categories}>
+        <button>
+          <span>Playlists</span>
+        </button>
+        <button>
+          <span>Podcasts & Shows</span>
+        </button>
+      </div>
+      <div className={styles.lib_content}>
+        <div className={styles.search_filter}>
+          <button>
+            <Icon name="search" size={16} />
+          </button>
+          <div className={styles.dropdown}>
+            Recents <Icon name="dropDown" size={16} />
+          </div>
+        </div>
+      </div>
+      <ul role="list">
+        {playlists.map((i) => {
           return (
             <li key={i.id}>
-                <Image src={i.img_url} width={45} height={45} alt="" />
-                <div className={styles.item}>
-                    <span className={styles.name}>{i.name}</span>
-                    <div className={styles.details}>
-                        <span>{i.type}</span>
-                        &bull;
-                        <span>{i.author}</span>
-                    </div>
+              <span className={styles.img_con}>
+                <Image src={i.images[0].url} fill alt="" />
+              </span>
+              <div className={styles.item}>
+                <span className={styles.name}>{i.name}</span>
+                <div className={styles.details}>
+                  <span>{i.type}</span>
+                  &bull;
+                  <span>{i.owner.display_name}</span>
                 </div>
+              </div>
             </li>
           );
         })}
-      </ul> */}
+      </ul>
     </div>
   );
-};
-
+}
