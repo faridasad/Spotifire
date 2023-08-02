a73c9583e0fdc5ab1e641a3f5c771cc8ed8006d9
faridasad <asadlifaridd@gmail.com>
2023-08-02 17:48:17 +0400

    Disable eslint for now

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ['i.scdn.co', 'hips.hearstapps.com', 'upload.wikimedia.org', 'images-ak.spotifycdn.com', 'mosaic.scdn.co']
-    }
-}
+  images: {
+    domains: [
+      "i.scdn.co",
+      "hips.hearstapps.com",
+      "upload.wikimedia.org",
+      "images-ak.spotifycdn.com",
+      "mosaic.scdn.co",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
