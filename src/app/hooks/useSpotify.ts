e8f4b2bdde4ee55ac360c323c3d1b6f010c38e08
faridasad <asadlifaridd@gmail.com>
2023-08-02 17:18:36 +0400

    fix

--- src/app/hooks/useSpotify.ts
+++ src/app/hooks/useSpotify.ts
@@ -1,15 +1,16 @@
 "use client";
 
 import { signIn, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Session } from "next-auth";
 import SpotifyWebApi from "spotify-web-api-node";
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+});
+
 const useSpotify = () => {
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
-  });
 
   const { data: session }: any = useSession();
 
