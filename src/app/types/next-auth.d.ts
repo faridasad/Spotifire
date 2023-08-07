1de379c5aafbd8cf87916aaca8fda27adc5a05ee
faridasad <asadlifaridd@gmail.com>
2023-08-07 22:19:40 +0400

    Update session types

--- src/app/types/next-auth.d.ts
+++ src/app/types/next-auth.d.ts
@@ -1,9 +1,13 @@
-import { DefaultUser } from 'next-auth';
-declare module 'next-auth' {
-    interface Session {
-        user?: DefaultUser & { accessToken?: string | null };
-    }
-    interface User extends DefaultUser {
-        accessToken?: string | null;
-    }
+import { DefaultUser } from "next-auth";
+declare module "next-auth" {
+  interface Session {
+    user?: DefaultUser & {
+      accessToken?: string | null;
+      accountId?: string | null;
+    };
+  }
+  interface User extends DefaultUser {
+    accessToken?: string | null;
+    accountId?: string | null;
+  }
 }
