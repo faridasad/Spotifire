870bd44090be1ee0898240b030033ae4585a3a77
faridasad <asadlifaridd@gmail.com>
2023-08-09 16:30:56 +0400

    Augment session

--- src/app/types/next-auth.d.ts
+++ src/app/types/next-auth.d.ts
@@ -1,13 +1,25 @@
-import { DefaultUser } from "next-auth";
+import { DefaultUser, DefaultSession } from "next-auth";
+import { JWT, DefaultJWT } from "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
-    user?: DefaultUser & {
-      accessToken?: string | null;
-      accountId?: string | null;
+    user: DefaultUser & {
+      accessToken?: string;
+      refreshToken?: string;
+      accountId?: string;
     };
   }
   interface User extends DefaultUser {
-    accessToken?: string | null;
-    accountId?: string | null;
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
+
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken: string,
+    refreshToken: string,
+    accountId: string,
+    accessTokenExpiresAt: number;
   }
 }
