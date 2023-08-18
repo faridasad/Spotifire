16737ac854a2172fa58e4dee63bc5689759ee5d1
faridasad <asadlifaridd@gmail.com>
2023-08-18 17:19:35 +0400

    Search

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,8 +1,23 @@
-'use client'
-import Icon from "../Icons"
-import styles from "./searchbar.module.scss"
+"use client";
+import { ChangeEvent, useRef } from "react";
+import Icon from "../Icons";
+import styles from "./searchbar.module.scss";
+import debounce from "lodash/debounce";
 
 function SearchBar() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const debouncedHandleSearch = debounce(async (query: string | undefined) => {
+    if (query && query.length >= 3) {
+      console.log(query);
+    }
+  }, 500);
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const query = inputRef.current?.value;
+    debouncedHandleSearch(query);
+  };
+
   return (
     <div className={styles.search_bar}>
       <div>
@@ -11,12 +26,14 @@ function SearchBar() {
         </span>
       </div>
       <input
+        ref={inputRef}
         type="text"
         placeholder="What do you want to listen to?"
         spellCheck="false"
+        onChange={handleSearch}
       />
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
