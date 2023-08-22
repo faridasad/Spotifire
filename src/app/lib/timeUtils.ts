2ca596c005c024201546efab7418c4a6d42e1a4b
faridasad <asadlifaridd@gmail.com>
2023-08-22 16:22:53 +0400

    Remove duplication

--- src/app/lib/timeUtils.ts
+++
@@ -1,8 +0,0 @@
-export function formatTimeFromMs(ms: number) {
-    const ts = Math.floor(ms / 1000)
-    const m = Math.floor(ts / 60)
-    const s = ts % 60
-
-    return `${m}:${s.toString().padStart(2, '0')}`;
-
-}
